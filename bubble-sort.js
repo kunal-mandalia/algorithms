f00031ad0d4dcd84904e98301aea49c997f8788f
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-02-29 02:44:27 +0000

    refactor: make synchronous

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,19 +1,17 @@
-const bubbleSort = inputArray => {
-  return new Promise(resolve => {
-    const sort = arr => {
-      let didSwap = false
-      for (let i = 0; i < arr.length - 1; i++) {
-        const l = arr[i]
-        const r = arr[i+1]
-        if (l > r) {
-          inputArray.splice(i, 2, r, l)
-          didSwap = true
-        }
+const bubbleSort = arr => {
+  let didSwap = true
+  while (didSwap) {
+    didSwap = false
+    for (let i = 0; i < arr.length - 1; i++) {
+      const l = arr[i]
+      const r = arr[i+1]
+      if (l > r) {
+        arr.splice(i, 2, r, l)
+        didSwap = true
       }
-      didSwap ? sort(inputArray) : resolve(inputArray)
     }
-    sort(inputArray)
-  })
+  }
+  return arr
 }
 
 module.exports = bubbleSort
--- bubble-sort.test.js
+++ bubble-sort.test.js
@@ -1,11 +1,11 @@
 const bubbleSort = require("./bubble-sort.js")
 
 describe("bubble-sort", () => {
-  it("should sort an unsorted array of numbers ascendingly", async () => {
+  it("should sort an unsorted array of numbers ascendingly", () => {
     // assign
     const inputArray = [6,3,2,5,4,1]
     // act
-    const result = await bubbleSort(inputArray)
+    const result = bubbleSort(inputArray)
     // assert
     expect(result).toEqual([1,2,3,4,5,6])
   })
