db97d17786553ffc24fe89bfa60bcddadd766858
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-03-03 21:32:55 +0000

    feat: add paginate message algorithm

---
+++ paginate-messages.test.ts
@@ -0,0 +1,22 @@
+import paginateMessages from './paginate-messages'
+
+describe('paginate messages', () => {
+  const cases = [
+    {
+      s: 'The quick brown fox',
+      maxSize: 5,
+      expected: ['The', 'quick', 'brown', 'fox']
+    },
+    {
+      s: 'The first person on the Moon was',
+      maxSize: 12,
+      expected: ['The first', 'person on', 'the Moon was']
+    }
+  ]
+
+  cases.forEach(c => {
+    it(`should return ${c.expected} given (s: ${c.s}, maxSize: ${c.maxSize})`, () => {
+      expect(paginateMessages(c.s, c.maxSize)).toEqual(c.expected)
+    })
+  })
+})
