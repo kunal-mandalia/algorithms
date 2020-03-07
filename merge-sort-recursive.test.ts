cb69c40b026201cc1419d2d923e08fc91ba33416
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-03-07 13:58:27 +0000

    feat: handle edge cases

--- merge-sort-recursive.test.ts
+++ merge-sort-recursive.test.ts
@@ -1,4 +1,4 @@
-import mergeSort from './merge-sort-recursive'
+import mergeSort, { mergePair } from './merge-sort-recursive'
 
 describe('merge sort (recursive)', () => {
   describe('should sort an unsorted array of numbers', () => {
@@ -24,6 +24,53 @@ describe('merge sort (recursive)', () => {
         // assert
         expect(mergeSort(c.input)).toEqual(c.expected)
       })
+    })
+  })
+})
+
+
+describe('merge pair (recursive)', () => {
+  const cases = [
+    {
+      left: [1,2],
+      right: [3,4],
+      expected: [1,2,3,4]
+    },
+    {
+      left: [1,4],
+      right: [2,3],
+      expected: [1,2,3,4]
+    },
+    {
+      left: [3,4],
+      right: [1,2],
+      expected: [1,2,3,4]
+    },
+    {
+      left: [1,3,5],
+      right: [2,4,6],
+      expected: [1,2,3,4,5,6]
+    },
+    {
+      left: [1,3],
+      right: [2,4,5],
+      expected: [1,2,3,4,5]
+    },
+    {
+      left: [3],
+      right: [1,2,4,5],
+      expected: [1,2,3,4,5]
+    },
+    {
+      left: [],
+      right: [1,2,3,4,5],
+      expected: [1,2,3,4,5]
+    }
+  ]
+
+  cases.forEach(c => {
+    it(`should return ${c.expected} given left ${c.left}, right ${c.right}`, () => {
+      expect(mergePair(c.left, c.right)).toEqual(c.expected)
     })
   })
 })
--- merge-sort-recursive.ts
+++ merge-sort-recursive.ts
@@ -6,18 +6,33 @@
  * @param leftIndex index to compare to right
  * @param rightIndex index to compare to left
  */
-function mergePair(left: number[], right: number[], leftIndex: number = 0, rightIndex: number = 0) {
-  if (left[leftIndex] > right[rightIndex]) {
-    left.splice(leftIndex, 0, right[rightIndex])
-    right.splice(rightIndex, 1)
+export function mergePair(left: number[], right: number[], i: number = 0, j: number = 0) {
+  // since `left` and `right` are sorted arrays if `left` is empty return `right`
+  if (left.length === 0) {
+    return right
   }
-  if (leftIndex < left.length - 1) {
-    return mergePair(left, right, leftIndex + 1, rightIndex)
+
+  // items are removed from `right` once they are sorted and inserted into `left`
+  // if no items remain then we are done
+  if (right.length === 0) {
+    return left
   }
-  if (rightIndex < right.length - 1) {
-    return mergePair(left, right, leftIndex, rightIndex + 1)
+
+  if (left[i] > right[j]) {
+    left.splice(i,0,right[j])
+    right.splice(j,1)
+    // there is always a next item in `left` since we have inserted an element from `right`
+    return mergePair(left, right, i+1, j)
   }
-  return [...left, ...right]
+
+  // the largest number in `left` is smaller than the smallest number in `right`
+  // therefore both are sorted relative to each other so flatten and return
+  if (i === left.length - 1) {
+    return [...left,...right] 
+  }
+
+  // compare the next largest number from `left` since the current number is smaller than `right`
+  return mergePair(left, right, i+1, j)
 }
 
 /**
