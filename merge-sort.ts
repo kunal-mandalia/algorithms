b1b1e1195c7c52a8d696a17bcbb2fa6276a5a240
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-03-08 22:02:15 +0000

    style: spacing

--- merge-sort.ts
+++ merge-sort.ts
@@ -16,7 +16,7 @@ export default function mergeSort(input: number[]) {
 }
 
 /**
- * Merge two arrays
+ * Merge two sorted arrays in sorted order
  * 
  * Time complexity: O(l+r) where l is the length of left array and r is the length of the right array
  * Space complexity: O(l+r)
@@ -26,15 +26,11 @@ export default function mergeSort(input: number[]) {
  */
 export function merge(left: number[], right: number[]) {
   let result = []
-  let i = 0
-  let j = 0
+  let i = 0 // index cursor over left array
+  let j = 0 // index cursor over right array
 
-  if (!left.length) {
-    return right
-  }
-  if (!right.length) {
-    return left
-  }
+  if (left.length === 0) return right
+  if (right.length === 0) return left
 
   while (result.length < left.length + right.length) {
     if (left[i] < right[j]) {
@@ -43,7 +39,7 @@ export function merge(left: number[], right: number[]) {
       // from the right array to the result because all remaining elements of the sorted right
       // array are greater than what's in the result array
       if (i === left.length - 1) {
-        result.push(...right.slice(j,right.length))
+        result.push(...right.slice(j, right.length))
       }
       i++
     } else {
@@ -52,7 +48,7 @@ export function merge(left: number[], right: number[]) {
       // from the left array to the result because all remaining elements of the sorted left
       // array are greater than what's in the result array
       if (j === right.length - 1) {
-        result.push(...left.slice(i,left.length))
+        result.push(...left.slice(i, left.length))
       }
       j++
     }
